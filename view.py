ab24802a27bf9d89d162386530be5a43f1733cdb
dkarmy12 <55491427+dkarmy12@users.noreply.github.com>
2021-04-29 20:14:36 +0530

    Removed old code

--- view.py
+++ view.py
@@ -21,25 +21,6 @@ def serve_layout():
         figure=fig)])
 
 app.layout = serve_layout
-# app.layout = html.Div(children=[
-#     html.H1(children='Progress count'),
-
-#     dcc.Graph(
-#         id='Count',
-#         dcc.Interval(
-#             id='interval-component',
-#             interval=2*60*1000, # in milliseconds , here 2 minutes
-#             n_intervals=0
-#         )
-#     )
-# ])
-
-# @app.callback(Output('live-update-graph', 'figure'),
-#               Input('interval-component', 'n_intervals'))
-# def update_graph_live(n):
-#     df = pd.read_pickle("data.pickle")
-#     fig = px.scatter(df, x="Date", y="Count", color="User", size="Count")
-#     return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
